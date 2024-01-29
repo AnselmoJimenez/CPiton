4435cc1109db5fc996b9b27872633f6ee672cd8c
AnselmoJimenez <anselmojimenez0@gmail.com>
2024-01-29 10:59:06 -0500

    fixed building script

--- build.sh
+++ build.sh
@@ -3,39 +3,36 @@
 # executable name
 output="Piton"
 
-# Specify the source and include directories
-src_dir="src"
-include_dir="include"
-
 # Create a build directory if it doesnt exist
 build_dir="build"
 mkdir -p "$build_dir"
 
 # Source files and object files
-source_files="$src_dir/*.c"
-object_files=""
-for file in $source_files;
-do
-    o_file="$build_dir/$(basename "$file" .c).o"
-    object_files="$object_files $o_file"
-done
+source_files="src/*.c"
+include_files="include/*.h"
 
 # Compiler and flags
 cc="gcc"
-cflags="-Wall -Wextra -I$include_dir -c"
+cflags="-Wall -Wextra"
 
-# Compile source files and object files
-for file in $source_files;
+echo "Compiling source files"
+object_files=""
+for file in $(basename -s .c $source_files);
 do
-    o_file="$build_dir/$(basename "$file" .c).o"
-    $cc $cflags $file -o $o_file
+    # Logging
+    echo "  $cc -c src/$file.c -o $build_dir/$file.o -Iinclude"
+
+    $cc -c src/$file.c -o $build_dir/$file.o -Iinclude
+    object_files="$object_files $build_dir/$file.o"
 done
 
-# Link object files to create the final executable
-$cc $object_files -o "$output"
+# Link all object files
+echo "Linking object files"
+echo "  $cc $object_files -o $output"
+$cc $object_files -o $output
 
-# Check if compilation was successful
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]; 
+then
     echo "Compilation successful."
 else
     echo "Compilation failed."
